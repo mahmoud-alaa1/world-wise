292336dc35f826d61ffc1e0f521cb339b1b7df54
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-05 00:28:49 +0300

    Refactor
    1. I replaced my pages with the pages from starter file
    2. I got the logo and and pagenav from starter files componenets

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,19 +1,21 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
-
+import Logo from "./Logo";
 function PageNav() {
-  console.log(styles)
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to={"/"}>Home Page</NavLink>
-        </li>
         <li>
           <NavLink to={"/pricing"}>Pricing</NavLink>
         </li>
         <li>
           <NavLink to={"/product"}>product</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/login"} className={styles.ctaLink}>
+            Log in
+          </NavLink>
         </li>
       </ul>
     </nav>
