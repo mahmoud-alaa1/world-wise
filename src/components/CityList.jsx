f615b57e3e26781454a3361cb8723efad2227a57
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-05 04:53:31 +0300

    Feature CityList Component
    if there is no cities print message

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,9 +2,12 @@ import styles from "./CityList.module.css";
 
 import Spinner from "./Spinner";
 import CityItem from "./CityItem";
+import Message from "./Message";
 
 function CityList({ cities, loading }) {
   if (loading) return <Spinner />;
+  if (!cities.length)
+    return <Message message={"Add your first city by clicking on the map"} />;
   return (
     <ul className={styles.cityList}>
       {cities.map((city, i) => (
