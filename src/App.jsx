cc4836eee66eff41ad06829f0bab8d7d61941096
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-05 03:03:26 +0300

    Create
    Create city component and inject it to its route component in app

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
 
 function App() {
   return (
@@ -15,8 +16,8 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of citites</p>} />
-          <Route path="cities" element={<p>List of citites</p>} />
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
           <Route path="countries" element={<p>List of counteries</p>}></Route>
           <Route path="form" element={<p>form</p>}></Route>
         </Route>
