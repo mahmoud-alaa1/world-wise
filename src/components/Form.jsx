dcf66dcaecaefa3b8208d2027aafec2dfe6a5d1b
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-06 21:53:54 +0300

    Fix Bug
    because button is inside form tag the button will act as a submit form so excute preventDefault to fix this issue

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -51,7 +51,15 @@ function Form() {
 
       <div className={styles.buttons}>
         <Button type={`primary`}>Add</Button>
-        <Button type={`back`} onClick={()=>navigate(-1)}>&larr; Back</Button>
+        <Button
+          type={`back`}
+          onClick={(e) => {
+            e.preventdefault();
+            navigate(-1);
+          }}
+        >
+          &larr; Back
+        </Button>
       </div>
     </form>
   );
