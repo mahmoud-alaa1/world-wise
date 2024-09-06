7291541a8906c02ae3a54f1ee3a1e4fe307bc1c7
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-06 22:00:50 +0300

    fix bug
    use keyword 'replace' so don't erase browser history

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<Navigate to={"cities"} />} />
+          <Route index element={<Navigate replace to={"cities"} />} />
 
           <Route
             path="cities"
