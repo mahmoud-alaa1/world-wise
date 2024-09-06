f1e432a65f030494ed1fcf192e82d7c46da3f4d2
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-06 21:59:52 +0300

    Feature
    Entering the app now redicrects you to the cities path automatically

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -24,10 +24,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} loading={loading} />}
-          />
+          <Route index element={<Navigate to={"cities"} />} />
 
           <Route
             path="cities"
