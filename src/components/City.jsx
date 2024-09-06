b940e4f7ab414e1833ae80c2c146125e2c5fd7f7
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-06 04:53:27 +0300

    New Hook
    useSearchParams

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -21,7 +21,18 @@ function City() {
   };
 
   const { cityName, emoji, date, notes } = currentCity;
-  return <h1>City {id}</h1>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <>
+      <h1>City {id}</h1>
+      <p>
+        
+        Position: {lat}, {lng}
+      </p>
+    </>
+  );
   // return (
   //   <div className={styles.city}>
   //     <div className={styles.row}>
