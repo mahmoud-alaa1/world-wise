4593c326eeee0460ab1d168974876197c723c6d8
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-10 06:35:06 +0300

    Style : hover
    Home page link has hover effect
    
    logo  has hover effect

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import styles from "./Logo.module.css";
 
 function Logo() {
   return (
-    <Link to="/">
+    <Link to="/" className={styles.link}>
       <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
     </Link>
   );
